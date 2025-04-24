e2e81d36bebf4faf18b8cfff9e0bb275364df907
shashank-jh <125866173+shashank-jh@users.noreply.github.com>
2025-04-24 18:46:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.write('You are  in AWS  Cloud. This is SHASHANK JH from ZuciTech, Please share and Subscribe and like my Videos'); //write a response to the client
   res.end(); //end the response
-}).listen(3000); //the server object listens on port 5000
+}).listen(4000); //the server object listens on port 4000
